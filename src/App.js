89c501525ff4b9e9411045542945cb50a1646e3b
suniLPATIDARr <sunilpatidar1209@gmail.com>
2021-03-25 15:33:01 +0530

    added action and reducer

--- src/App.js
+++ src/App.js
@@ -3,53 +3,51 @@ import "./App.css";
 
 //its a function that returns higer order compoent
 import { connect } from "react-redux";
+import logo from "./logo.svg";
+import * as actionCreator from "./store/actions/actions";
 
 class App extends Component {
   render() {
     return (
-      <div className="App" >
+      <div className="App">
         <div className="header">
-          <div className="age" >
-          <h3> Your age: {this.props.age}</h3>
+          <div className="age">
+            <h3> Your age: {this.props.age}</h3>
           </div>
-          <button className="ageUp" style={{background:"green"}} onClick={this.props.onAgeUp}>
+          <button
+            className="ageUp"
+            style={{ background: "green" }}
+            onClick={this.props.onAgeUp}
+          >
             Age UP
           </button>
-          <button className="ageDown" style={{background:"red"}} onClick={this.props.onAgeDown}>
+          <button
+            className="ageDown"
+            style={{ background: "red" }}
+            onClick={this.props.onAgeDown}
+          >
             Age Down
           </button>
-        </div>
-        <div style={{border:"2px solid red",padding:20,boxSizing:"border-box"}}><h1>History</h1>
-        <div>
-          <ul>
-            {this.props.history.map(el => (
-              <li 
-                className="historyItem" 
-                key={el.id}
-                onClick={() => this.props.onDelEntry(el.id)}>
-                {el.age}
-              </li>
-            ))}
-          </ul>
-        </div>
+          {this.props.loading && <img src={logo} className="App-logo" />}
         </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = state => { 
+const mapStateToProps = (state) => {
   return {
     age: state.age,
-    history: state.history
+    history: state.history,
+    loading: state.loading,
   };
 };
 
-const mapDispachToProps = dispatch => {
+const mapDispachToProps = (dispach) => {
   return {
-    onAgeUp: () => dispatch({ type: "AGE_UP", value: 1 }),
-    onAgeDown: () => dispatch({ type: "AGE_DOWN", value: 1 }),
-    onDelEntry: (id) => dispatch({ type: "DEL_ENTRY", id: id})
+    onAgeUp: () => dispach(actionCreator.ageUp(1)),
+    onAgeDown: () => dispach(actionCreator.ageDown(1)),
+    onDelEntry: (id) => dispach({ type: "DEL_ENTRY", id: id }),
   };
 };
 export default connect(
